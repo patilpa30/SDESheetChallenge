93f1416671020f65382365a0d1129b116f2ad9f7
PRITI PATIL <123315757+patilpa30@users.noreply.github.com>
2026-06-01 23:43:43 +0530

    Create Next-Permutation.java

---
+++ Next-Permutation.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int idx = -1;
+        for(int i = n-2; i>=0;i--){
+            if(nums[i] < nums[i+1]){
+                idx = i;
+                break;
+            }
+        }
+        if(idx == -1){
+            reverse(nums, idx + 1, n - 1);
+            return;
+        }
+        for(int i = n-1; i>=0;i--){
+            if(nums[i] > nums[idx]){
+               swap(nums, idx, i);
+                break;
+            }
+        }
+        reverse(nums, idx + 1, n-1);
+        return;
+    }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
