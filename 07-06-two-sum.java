8753770a9c473270df962f6b6897a6c41d09c886
PRITI PATIL <123315757+patilpa30@users.noreply.github.com>
2026-06-07 00:58:03 +0530

    Create 07-06-two-sum.java

---
+++ 07-06-two-sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> mpp= new HashMap<>();
+
+        for(int i= 0; i< nums.length;i++){
+            int rem = target-nums[i];
+            if(mpp.containsKey(rem)){
+                return new int[]{mpp.get(rem), i};
+            }
+            mpp.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+}
